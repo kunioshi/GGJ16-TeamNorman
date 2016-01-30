68676909f50ce79b60e2d2019d97bc5a7056ef3b
dangeroustuna <gaccount@126.com>
2016-01-30 09:17:36 -0800

    nightScene, drag n drop INTO to magical circle, graphic only, no inventory involvement, drag out not done yet

--- Assets/Scripts/MagicCircle.cs
+++ Assets/Scripts/MagicCircle.cs
@@ -5,12 +5,22 @@ using UnityEngine.UI;
 public class MagicCircle : MonoBehaviour
 {
 	float edgeLength;
+	// a list of each type of rune
+	public Rune[] runeList;
+	Vector3[] runePositions = new Vector3[4];
+
+	private Rune current;
 	public RuneSlot[] slots;
 	public RuneSlot[] minorSlots;
+	AudioSource sfx;
 	// Use this for initialization
 	void Start ()
 	{
-		Debug.Log ("slots array size: " + slots.Length);
+		sfx = GetComponent<AudioSource> ();
+		sfx.enabled = false;
+		for (int i = 0; i < runeList.Length; i++) {
+			runePositions [i] = runeList [i].transform.position;
+		}
 		RectTransform rectTrans = gameObject.GetComponent<RectTransform> ();
 		edgeLength = rectTrans.rect.height * 0.425f;
 
@@ -63,6 +73,65 @@ public class MagicCircle : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
 	{
-	
+
+		//put in new rune
+		if (Input.GetMouseButtonDown (0)) {
+			for (int i = 0; i < 4; i++) {
+				//Debug.Log("Distance :")
+				if (isNear (runeList [i].transform.position, Input.mousePosition)) {
+					current = runeList [i];
+					sfx.enabled = true;
+				}
+			}
+		}
+
+		if (Input.GetMouseButton (0) && current != null) {
+			current.transform.position = Input.mousePosition;
+		}
+
+		if (Input.GetMouseButtonUp (0)) { 
+			sfx.enabled = false;
+			if (current != null) {
+				//check minor slots
+				for (int i = 0; i < 8; i++) {
+					if (isNear (minorSlots [i], Input.mousePosition)) {
+						Rune temp = (Rune)Instantiate (current, minorSlots [i].transform.position, Quaternion.identity);
+						temp.transform.SetParent (minorSlots [i].transform);
+						if (minorSlots [i].rune != null) {
+							Destroy (minorSlots [i].rune.gameObject);
+						}
+						minorSlots [i].rune = temp;
+					}		
+				}
+				//check major slots
+				for (int i = 0; i < 8; i++) {
+					if (isNear (slots [i], Input.mousePosition)) {
+						Rune temp = (Rune)Instantiate (current, slots [i].transform.position, Quaternion.identity);
+						temp.transform.SetParent (slots [i].transform);
+						if (slots [i].rune != null) {
+							Destroy (slots [i].rune.gameObject);
+						}
+						slots [i].rune = temp;
+					}		
+				}
+
+				current.transform.position = runePositions [current.id];
+			}
+
+			current = null;
+		}
+
+	}
+
+	//given a position and a runslot position, return ture if they are close to each other
+	public bool isNear (RuneSlot rs, Vector3 position)
+	{
+		return isNear (rs.transform.position, position);
+	}
+
+	//given 2 positions, return ture if they are close to each other
+	public bool isNear (Vector3 position0, Vector3 position)
+	{
+		return (position0 - position).magnitude < 20;
 	}
 }
--- Assets/Scripts/RuneSlot.cs
+++ Assets/Scripts/RuneSlot.cs
@@ -4,16 +4,21 @@ using UnityEngine.UI;
 
 public class RuneSlot : MonoBehaviour
 {
+	//default image of empty
 	public Image img;
+	//current rune on it
+	public Rune rune;
+
+
+
 	// Use this for initialization
 	void Start ()
 	{
-	
+
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-	
 	}
 }
