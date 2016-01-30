0e9e5152d2220d3b58016a81ce88b54c98514026
NorW <norman_wong@live.com>
2016-01-30 04:42:56 -0800

    Implemented Tile and TileManager functionality.

--- Assets/Scripts/TileManager.cs
+++ Assets/Scripts/TileManager.cs
@@ -4,7 +4,18 @@ using System.Collections.Generic;
 
 public struct Vector2i
 {
-    int x, y;
+    public int x, y;
+
+    public Vector2i(int xVal, int yVal)
+    {
+        x = xVal;
+        y = yVal;
+    }
+
+    public int manhattandistance(Vector2i other)
+    {
+        return Mathf.Abs(x - other.x) + Mathf.Abs(y - other.y);
+    }
 
     public override bool Equals(object obj)
     {
@@ -37,25 +48,280 @@ public class Tile
         Cave,
         Lake,
         Graveyard,
-        Volcano
+        Volcano,
+        N_TILE_TYPES
     };
+
+    public static int[] terrainPenalty = new int[(int)TileType.N_TILE_TYPES];
+
+    public static int[] defaultTerrainPenalties = new int[(int)TileType.N_TILE_TYPES];
 
     public TileType Type { get; private set; }
 
+    public Vector2i Position { get; private set; }
 
-    public Tile(TileType t)
+    public Tile[] neighbors = new Tile[4];
+
+    public Tile(TileType t, Vector2i position)
     {
         Type = t;
+        Position = position;
+        neighbors[(int)Direction.Up] = null;
+        neighbors[(int)Direction.Down] = null;
+        neighbors[(int)Direction.Left] = null;
+        neighbors[(int)Direction.Right] = null;
     }
 }
 
-public class TileManager : MonoBehaviour {
+public class TileManager : PersistentObject {
 
-    private Dictionary<Vector2i, Tile> _tiles;
+    private Dictionary<Vector2i, Tile> _tiles;  //Tiles that have been seen.
+    public int[] probabilities = new int[(int)Tile.TileType.N_TILE_TYPES];  //Probabilities of encountering each terrain type (MUST ADD UP TO 100)
+   
+
+    //Returns a random tile type based on probabilities (assumes probabilities adds up to 100).
+    public Tile.TileType getRandomType()
+    {
+        int value = (int)(Random.value * 100.0f);
+        int tileMaxValue = 0;
+
+        for(int i = 0; i < (int)Tile.TileType.N_TILE_TYPES; i++)
+        {
+            tileMaxValue += probabilities[i];
+            if(value <= tileMaxValue)
+            {
+                return (Tile.TileType)i;
+            }
+        }
+        return Tile.TileType.Plains;
+    }
+
+    //Returns the opposite direction of the direction d.
+    public static Direction oppositeDirection(Direction d)
+    {
+        switch(d)
+        {
+            case Direction.Up:
+                return Direction.Down;
+
+            case Direction.Down:
+                return Direction.Up;
+
+            case Direction.Left:
+                return Direction.Right;
+
+            case Direction.Right:
+                return Direction.Left;
+
+            default:
+                return Direction.Up;
+        }
+    }
+
+    //Returns the tile at the position "position" if it exists. If it doesnt exist, returns null.
+    public Tile getTile(Vector2i position)
+    {
+        if(_tiles.ContainsKey(position))
+        {
+            return _tiles[position];
+        }
+        return null;
+    }
+
+    //Links the tile t with the tile located at position pos, where d is the direction pos is located in relative to tile t.
+    private void linkTiles(Tile t, Vector2i pos, Direction d)
+    {
+        if (_tiles.ContainsKey(pos))
+        {
+            t.neighbors[(int)d] = _tiles[pos];
+            _tiles[pos].neighbors[(int)oppositeDirection(d)] = t;
+        }
+        else
+        {
+            t.neighbors[(int)d] = null;
+        }
+    }
+
+    //Like the tile t with its neighbors.
+    public void linkTile(Tile t)
+    {
+        Vector2i neighborPos;
+
+        neighborPos = t.Position;
+        neighborPos.y += 1;
+        linkTiles(t, neighborPos, Direction.Up);    //Link neighbhor above this tile
+
+        neighborPos.y -= 2;
+        linkTiles(t, neighborPos, Direction.Down);  //Link neighbor below this tile
+
+        neighborPos.y += 1;
+        neighborPos.x += 1;
+        linkTiles(t, neighborPos, Direction.Right); //Link neighbor right of this tile
+
+        neighborPos.x -= 2;
+        linkTiles(t, neighborPos, Direction.Left);  //Link neighbor left of this tile
+    }
+
+    //Returns the shortest path from start to dest. If the dest can not be reached befor remainingEnergy runs out (or for other reasons), returns null.
+    public List<Tile> getPath(Vector2i start, Vector2i dest, int remainingEnergy)
+    {
+        if(!_tiles.ContainsKey(dest) || !_tiles.ContainsKey(start) || start.manhattandistance(dest) > remainingEnergy)
+        {
+            return null;
+        }
+
+        List<Tile> path = new List<Tile>();
+
+        Dictionary<Tile, int> distanceFromStart = new Dictionary<Tile, int>();  //Minimum distance from start to each tile.
+        Dictionary<Tile, Tile> tileOrigins = new Dictionary<Tile, Tile>();  //Originating tile. (Also acts as a closed list).
+        List<Tile> openNodes = new List<Tile>();
+        List<Tile> closedNodes = new List<Tile>();
+
+        openNodes.Add(_tiles[start]);
+        tileOrigins[_tiles[start]] = _tiles[start];
+        distanceFromStart[_tiles[start]] = 0;
+
+        Tile curNode, curNeighbor;
+        int curDistance;
+
+        //While there are still open nodes
+        while(openNodes.Count > 0)
+        {
+            //Pop the first node
+            curNode = openNodes[0];
+            openNodes.RemoveAt(0);
+            
+            //For each of its neighbors, get the distance to that neighbor if it exists, and add the distance from start to the current node to that distance to get the distance from start to that neighbor through the current node.
+            for(int i = 0; i < 4; i++)
+            {
+                curNeighbor = curNode.neighbors[i];
+
+                if(curNeighbor == null)
+                {
+                    continue;
+                }
+
+                curDistance = Tile.terrainPenalty[(int)curNeighbor.Type] + distanceFromStart[curNode];
+
+                //If the neighbor has already been found, if the distance through the cur tile to the neighbor is less than the current known shortest distance, change the neighbor's tile origin and distance.
+                if(tileOrigins.ContainsKey(curNeighbor) && distanceFromStart[curNeighbor] > curDistance)
+                {
+                    tileOrigins[curNeighbor] = curNode;
+                    distanceFromStart[curNeighbor] = curDistance;
+                }
+                else if(curDistance <= remainingEnergy) //Else if this is the first time the tile has been found, add it to open nodes and set the tile origins and distance from start.
+                {
+                    tileOrigins[curNeighbor] = curNode;
+                    distanceFromStart[curNeighbor] = curDistance;
+                    openNodes.Add(curNeighbor);
+                }
+                //If the neighbor can not physically be reached, do not add it to open nodes.
+            }
+        }
+
+        //If the destination tile was not reached, there is no path to that tile.
+        if(!tileOrigins.ContainsKey(_tiles[dest]))
+        {
+            return null;
+        }
+
+        curNode = _tiles[dest];
+        path.Add(curNode);
+
+        //Recreate the path.
+        while(curNode != _tiles[start])
+        {
+            curNode = tileOrigins[curNode];
+            path.Insert(0, curNode);
+        }
+        path.Insert(0, _tiles[start]);
+        return path;
+    }
+
+    //Create new tiles around the new position if they have nto already been created.
+    public void createTiles(Vector2i newPosition, int viewDistance)
+    {
+        HashSet<Tile> openTiles = new HashSet<Tile>();      //Current tiles to look through
+        HashSet<Tile> closedTiles = new HashSet<Tile>();    //Tiles to ignore (already traversed)
+        HashSet<Tile> curNeighbors = new HashSet<Tile>();   //Current neighbors (tiles next to the open tiles that are not in the closed tiles list.
+        openTiles.Add(_tiles[newPosition]);                 //Add initial tile
+        Tile curNeighbor;
+
+        //Only need to run this loop until it reaches as far as it can view.
+        for(int i = 0; i < viewDistance; i++)
+        {
+            //Go through each node and add its neighbor to curNeighbors if it exists, create a neighbhor there if it doesnt.
+            foreach(Tile curNode in openTiles)
+            {
+                for(int j = 0; j < 4; j++)
+                {
+                    curNeighbor = curNode.neighbors[j];
+
+                    //If there is no neighbor, create a new neighbor and link it.
+                    if(curNeighbor == null)
+                    {
+                        Vector2i neighborPosition = curNode.Position;
+                        switch((Direction)j)
+                        {
+                            case Direction.Up:
+                                neighborPosition.y += 1;
+                                break;
+
+                            case Direction.Down:
+                                neighborPosition.y -= 1;
+                                break;
+
+                            case Direction.Left:
+                                neighborPosition.x -= 1;
+                                break;
+
+                            case Direction.Right:
+                                neighborPosition.x += 1;
+                                break;
+                        }
+                        curNeighbor = new Tile(getRandomType(), neighborPosition);
+                        _tiles.Add(neighborPosition, curNeighbor);
+                        linkTile(_tiles[neighborPosition]);
+                    }
+                    else if(!closedTiles.Contains(curNeighbor)) //Else if the neighbor is not a closed tile, add it to cur neighbors (to prevent the code from going in circles).
+                    {
+                        curNeighbors.Add(curNeighbor);
+                    }
+                }
+            }
+
+            //Reassign closed and open tiles.
+            closedTiles.Clear();
+            closedTiles = openTiles;
+            openTiles.Clear();
+            openTiles = curNeighbors;
+        }
+    }
+
+    public void resetTerrainPenalties()
+    {
+        Tile.defaultTerrainPenalties.CopyTo(Tile.terrainPenalty, 0);
+    }
+
+    void Awake()
+    {
+        _tiles = new Dictionary<Vector2i, Tile>();  //Create a new tile dictionary
+        Vector2i initialPosition = new Vector2i(0, 0);  //Set initial position
+        _tiles.Add(initialPosition, new Tile(Tile.TileType.Plains, initialPosition));   //Create tile at initial position
+        createTiles(initialPosition, 3);    //Create tiles around initial position. TODO: get view distance
+        Tile.defaultTerrainPenalties[0] = 1;
+        Tile.defaultTerrainPenalties[1] = 2;
+        Tile.defaultTerrainPenalties[2] = 3;
+        Tile.defaultTerrainPenalties[3] = 2;
+        Tile.defaultTerrainPenalties[4] = 2;
+        Tile.defaultTerrainPenalties[5] = 2;
+        Tile.defaultTerrainPenalties[6] = 4;
+        resetTerrainPenalties();
+    }
 
 	// Use this for initialization
 	void Start () {
-	
+	    
 	}
 	
 	// Update is called once per frame
