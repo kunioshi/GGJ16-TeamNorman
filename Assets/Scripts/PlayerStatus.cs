ede976da0b1f2c9cc38afd42f4f7ae611215f997
jh1993 <johnnyhan1993@gmail.com>
2016-01-30 15:50:01 -0800

    Added bonuses and penalties in player status.

--- Assets/Scripts/PlayerStatus.cs
+++ Assets/Scripts/PlayerStatus.cs
@@ -4,20 +4,33 @@ using System.Collections.Generic;
 
 public enum RuneId
 {
-	Air = 0,
+	Life = 0,
 	Earth = 1,
-	Fire = 2,
-	Water = 3}
+	Death = 2,
+	Fire = 3}
 ;
 
 public class PlayerStatus: MonoBehaviour
 {
 	public Vector2i playerGridPosition = new Vector2i (0, 0);
 
-	public int playerEnergy = 120;
+	// The player starts each day with 40 energy by default
+	public int playerEnergy = 40;
 
 	// Initialize the rune inventory count with 8 runes for the tutorial sake
 	public int[] runeCounts = new int [4] { 2, 2, 2, 2 };
+
+	// Inidcates whether the player has a bonus in Life(0), Earth(1), Death(2), or Fire(3)
+	// Life: +1 vision radius
+	// Earth: 1.5x energy
+	//	Death: 2/3 energy cost on all tiles
+	//	Fire: 50% chance to get double resources per tile
+	public bool[] bonuses = new bool[4] {false, false, false, false};
+
+	// The number of ingredients missing in last night's ritual, from 0 to 8
+	// For each ingredient missing, the player takes an energy penalty of 5 the next day
+	// At a penalty of 8, the game ends
+	public int penalty = 0;
 
 	public void AddRuneToInventory (int runeId)
 	{
