3f66e2b6a317f6656f6abd9e7ac9c82671aef4c3
dangeroustuna <gaccount@126.com>
2016-01-31 09:31:55 -0800

    upperBar in both scene now info updated automatically, expect day counter, which should be updated only when switch day

--- Assets/Scripts/UpperBar.cs
+++ Assets/Scripts/UpperBar.cs
@@ -5,23 +5,49 @@ using UnityEngine.UI;
 public class UpperBar : MonoBehaviour
 {
 	public Image[] textShadows;
-
+	Text[] runeCounts = new Text[4];
+	public Text dayText;
+	public Text energyText;
+	public PlayerStatus playerStatus;
 	// Use this for initialization
 	void Start ()
 	{
+		playerStatus = GameObject.FindGameObjectWithTag ("GameController").GetComponent<PlayerStatus> ();
+		//set up inventory text position
 		for (int i = 0; i < 4; i++) {
 			textShadows [i].transform.position = new Vector3 (
 				Screen.width * 0.1f + i * Screen.width * 0.12f,
 				Screen.height * 0.96f,
 				0f);
-			textShadows [i].GetComponentInChildren<Text> ().transform.position = textShadows [i].transform.position + new Vector3 (0, -10, 0);
+			runeCounts [i] = textShadows [i].GetComponentInChildren<Text> ();
+			runeCounts [i].transform.position = textShadows [i].transform.position + new Vector3 (0, -10, 0);
 			;
 		}
+
+		//set up day text
+		dayText.transform.position = new Vector3 (
+			Screen.width * 0.675f,
+			Screen.height * 0.91f,
+			0f);
+
+		//set up energy text
+		energyText.transform.position = new Vector3 (
+			Screen.width * 0.925f,
+			Screen.height * 0.91f,
+			0f);
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-	
+		updateText ();
+	}
+
+	void updateText ()
+	{
+		for (int i = 0; i < 4; i++) {
+			runeCounts [i].text = playerStatus.runeCounts [i].ToString ();
+		}
+		energyText.text = playerStatus.playerEnergy.ToString ();
 	}
 }
