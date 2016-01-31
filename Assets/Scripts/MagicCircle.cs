7f375c94235c9d888a1801880ce66a912ff2113a
dangeroustuna <gaccount@126.com>
2016-01-31 14:01:16 -0800

    bug fix

--- Assets/Scripts/MagicCircle.cs
+++ Assets/Scripts/MagicCircle.cs
@@ -105,21 +105,25 @@ public class MagicCircle : MonoBehaviour
 		if (Input.GetMouseButtonDown (0)) {
 			//click on inventory
 			for (int i = 0; i < 4; i++) {
-				if (isNear (runeList [i].transform.position, Input.mousePosition) && playerStatus.runeCounts [i] > 0) {
-					current = runeList [i];
-					runeList [i] = null;
-					//pick up a rune: -1
-					playerStatus.RemoveRuneFromInventory (current.id);
-					break;
+				if (runeList [i] != null) {
+					if (isNear (runeList [i].transform.position, Input.mousePosition) && playerStatus.runeCounts [i] > 0) {
+						current = runeList [i];
+						runeList [i] = null;
+						//pick up a rune: -1
+						playerStatus.RemoveRuneFromInventory (current.id);
+						break;
+					}
 				}
 			}
 			// click on major slots
 			for (int i = 0; i < 8; i++) {
+
 				if (isNear (slots [i], Input.mousePosition) && slots [i].rune != null) {
 					current = slots [i].rune;
 					slots [i].rune = null;
 					break;
 				}
+
 			}
 
 			// click on minor slots
